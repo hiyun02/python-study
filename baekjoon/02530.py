8ec8560096c1da4e864c08ce28cbf066cbb0642f
hayoon020105 <psalter150@naver.com>
2021-09-24 23:58:37 +0900

    백준 파이썬 풀이 모음

---
+++ baekjoon/02530.py
@@ -0,0 +1,15 @@
+arr = list(map(int,input().split()))
+n=int(input())
+arr[2]+=n
+if(arr[2]>59) :
+    arr[1]+=arr[2]/60
+    arr[2]%=60
+if(arr[1]>59) :
+    arr[0]+=arr[1]/60
+    arr[1]%=60
+if(arr[0]>23) :
+    arr[0]%=24
+
+print(arr[0])
+print(arr[1])
+print(arr[2])
