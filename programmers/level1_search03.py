b3edb243aa5c8f07f9a7c18f1f8f6d50e6129f35
hayoon020105 <psalter150@naver.com>
2022-04-29 14:07:22 +0900

    프로그래머스 카펫

---
+++ programmers/level1_search03.py
@@ -0,0 +1,10 @@
+def solutuion(brown, yellow) :
+
+    ab=brown + yellow
+
+    for b in range(1,ab+1):
+        if(ab/b)%1 ==0:
+            a=ab/b
+            if a>=b :
+                if 2*a+2*b==brown+4:
+                    return [a,b]
